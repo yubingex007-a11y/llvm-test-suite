3800ffd32db07fd3b54c8bd8a8d0ef1f2ddf62e9
Whitney Tsang <54643204+whitneywhtsang@users.noreply.github.com>
2022-11-10 09:04:00 -0500

    [SYCL-MLIR] Add parallel_for.cpp (#1374)
    * [SYCL-MLIR] Add parallel_for.cpp
    
    Signed-off-by: Tsang, Whitney <whitney.tsang@intel.com>

--- SYCL/Basic/cgeist/parallel_for.cpp
+++ SYCL/Basic/cgeist/parallel_for.cpp
@@ -1,4 +1,4 @@
-// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple -Xcgeist -gen-all-sycl-funcs %s -o %t.out
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
 // RUN: %ACC_RUN_PLACEHOLDER %t.out
@@ -7,27 +7,30 @@
 
 #include <sycl/sycl.hpp>
 using namespace sycl;
+static constexpr unsigned N = 8;
 
-void host_single_task(std::array<int, 1> &A) {
+void host_parallel_for(std::array<int, N> &A) {
   auto q = queue{};
   device d = q.get_device();
   std::cout << "Using " << d.get_info<info::device::name>() << "\n";
+  auto range = sycl::range<1>{N};
 
   {
-    auto buf = buffer<int, 1>{A.data(), 1};
+    auto buf = buffer<int, 1>{A.data(), range};
     q.submit([&](handler &cgh) {
       auto A = buf.get_access<access::mode::write>(cgh);
-      cgh.single_task<class kernel_single_task>([=]() {
-        A[0] = 1;
+      cgh.parallel_for<class kernel_parallel_for>(range, [=](sycl::id<1> id) {
+        A[id] = id;
       });
     });
   }
 }
 
 int main() {
-  std::array<int, 1> A = {0};
-  A[0] = 0;
-  host_single_task(A);
-  assert(A[0] == 1);
+  std::array<int, N> A{0};
+  host_parallel_for(A);
+  for (unsigned i = 0; i < N; ++i) {
+    assert(A[i] == i);
+  }
   std::cout << "Test passed" << std::endl;
 }
